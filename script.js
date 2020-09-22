8fead4c949f474f2a87a2e544d92ef218067589e
collinreich <47579634+collinreich@users.noreply.github.com>
2020-09-22 15:21:36 -0400

    script.js for auth practice cpeg470

--- script.js
+++ script.js
@@ -1,141 +1,135 @@
-// Inspiration: http://jsfiddle.net/MK73j/4/
-// Making canvas look good: https://www.html5rocks.com/en/tutorials/canvas/hidpi/
-// Animation util lib: https://popmotion.io/popcorn/
-
-import popcorn from "https://jspm.dev/@popmotion/popcorn";
-console.clear();
-
-// canvas set up
-const canvas = document.querySelector("canvas");
-const dpr = window.devicePixelRatio || 1;
-const size = canvas.getBoundingClientRect();
-canvas.height = size.height * dpr;
-canvas.width = size.width * dpr;
-
-const ctx = canvas.getContext("2d");
-ctx.scale(dpr, dpr);
-
-window.addEventListener("resize", function (e) {
-  const canvas = document.querySelector("canvas");
-  const size = canvas.getBoundingClientRect();
-  canvas.height = size.height * dpr;
-  canvas.width = size.width * dpr;
-  ctx.scale(dpr, dpr);
-});
-
-ctx.fillStyle = "black";
-
-// utils
-const getRandomColorSet = () => {
-  const colorRanges = [
-  ["#FE7E9C", "#ff80df"],
-  ["#93f", "#ff80df"],
-  ["#d8dadb", "#53ceef"],
-  ["#FE7E9C", "#F7BD8D"]];
-
-  return colorRanges[Math.floor(Math.random() * colorRanges.length)];
+const firebaseConfig = {
+    apiKey: "AIzaSyCLONgiReZCAJjU4lXaoNRxph8g6EtoO5Q",
+    authDomain: "cpeg470-auth-practice.firebaseapp.com",
+    databaseURL: "https://cpeg470-auth-practice.firebaseio.com",
+    projectId: "cpeg470-auth-practice",
+    storageBucket: "cpeg470-auth-practice.appspot.com",
+    messagingSenderId: "221574779187",
+    appId: "1:221574779187:web:61eba811e6387ebedbae46"
 };
 
-// paint splat manager
-const Splat = function (options) {
-  const self = this;
+firebase.initializeApp(firebaseConfig);
 
-  let { x, y, event, colorSet } = options;
 
-  // set co-ordinate of event or set shape
-  if (typeof x === "undefined" || typeof y === "undefined") {
-    if (typeof event === "undefined") throw new Error("Must provide an origin");
-    x = event.clientX;
-    y = event.clientY;
-  }
-
-  // set color gradient
-  if (!colorSet) {
-    colorSet = ["#FF69B4", "#FFD700"];
-  }
-  self.colorMix = popcorn.mixColor(...colorSet);
-
-  self.particles = Array(72).
-  fill({}).
-  map((_, i) => {
-    const p = {
-      x,
-      y,
-      angle: i * 5,
-      size: 20 + Math.random() * 12.5,
-      life: 800 + Math.random() * 640 };
-
-    p.interpolator = popcorn.interpolate([p.size, 0], [0, 1]);
-    return p;
-  });
-
-  self.start = null;
-  let delta = 0;
-  let last = Date.now();
-
-  self.decay = t => {
-    delta = Date.now() - last;
-    last = Date.now();
-
-    self.particles = self.particles.reduce((result, p, i) => {
-      p.x += Math.cos(p.angle) * 4 + Math.random() * 2 - Math.random() * 2;
-      p.y += Math.sin(p.angle) * 4 + Math.random() * 2 - Math.random() * 2;
-
-      p.life -= delta;
-      p.size -= delta / 50;
-
-      if (p.size <= 0) p.life = 0;
-      if (p.life <= 0) return result;
-      result.push(p);
-      return result;
-    }, []);
-  };
-
-  self.render = () => {
-    self.particles.forEach((p, i) => {
-      if (Math.random() < 0.1) return;
-
-      ctx.fillStyle = `${self.colorMix(p.interpolator(p.size))}`;
-      ctx.beginPath();
-      ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2, false);
-      ctx.fill();
+firebase.auth().onAuthStateChanged(user => {
+  //alert('function called');
+  if (!!user){
+    //console.log(JSON.stringify(user));
+    $('#googleWrapper').html('');
+    $('#emailWrapper').html('');
+    $('#emailWrapper').append(`<h1>Success! Thank you, ${user.email}</h1>`);
+    //$('#wrapper').append(`<button class="signOut"> ${'Sign Out'} </button>`);
+    /* Broken right now
+    $('.signOut').addEventListener("click", function(event){
+      console.log("CATCH");
+      firebase.auth().signOut();
     });
-  };
-
-  self.animate = t => {
-    self.decay();
-    self.render();
-    if (self.particles.length > 0) {
-      window.requestAnimationFrame(self.animate);
-    }
-  };
-
-  self.fire = () => {
-    window.requestAnimationFrame(self.animate);
-  };
-
-  return {
-    fire: function () {
-      self.fire();
-    } };
-
-};
-
-canvas.addEventListener("mousedown", function (e) {
-  const s = new Splat({ event: e, colorSet: getRandomColorSet() });
-  s.fire();
+    */
+  }
+    //alert(`${user.displayName || user.email}`);
+  else{
+    alert('no user signed in.');
+  }
 });
 
-// timed demo
-const drawDemo = val => {
-  if (val < 1) return;
-  const x = Math.random() * size.width;
-  const y = Math.random() * size.height;
-  const s = new Splat({ x, y, colorSet: getRandomColorSet() });
-  s.fire();
-  setTimeout(() => {
-    drawDemo(val - 1);
-  }, 800);
+let provider = new firebase.auth.GoogleAuthProvider();
+
+let googleBtn = document.getElementById("googleBtn");
+
+googleBtn.addEventListener("click", function(event){
+  firebase.auth().signInWithRedirect(provider);
+});
+
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    let token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  let user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  let email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  let credential = error.credential;
+  // ...
+});
+
+/*
+firebase.auth()
+  .signInWithEmailAndPassword(
+    "junk@novocin.com", 
+    "fartfart"
+   ).catch(function(error) {
+          alert(error.message);
+   }
+);
+*/
+
+
+let sign = document.getElementById("submitBtn");
+let reg = document.getElementById("regBtn");
+
+let regUser = function(email, password){
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error){
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    switch(errorCode){
+      case 'auth/email-already-in-use':
+        alert('Email is already associated with another account.');
+        break;
+      case 'auth/invalid-email':
+        alert('Entered an invalid email address.');
+        break;
+      case 'auth/operation-not-allowed':
+        alert("Error code: " + errorCode + " Error message: " + errorMessage);
+        break;
+      case 'auth/weak-password':
+        alert('Password is too weak.');
+        break;
+      default:
+        alert("Error code: " + errorCode + " Error message: " + errorMessage);
+    }
+    console.log(error);
+  });
 };
 
-drawDemo(4);
+let signUser = function(email, password){
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    switch(errorCode){
+      case 'auth/invalid-email':
+        alert('Invalid email address.');
+        break;
+      case 'auth/user-disabled':
+        alert('Account has been disabled.');
+        break;
+      case 'auth/user-not-found':
+        alert('No account found associated with this email.');
+        break;
+      case 'auth/wrong-password':
+        alert("Incorrect email or password.");
+        break;
+      default:
+        alert("Error code: " + errorCode + " Error message: " + errorMessage);
+    }
+    console.log(error);
+  });
+}
+
+sign.addEventListener("click", function(event){
+  let email = document.getElementById("emailText").value;
+  let password = document.getElementById("passwordText").value;
+  signUser(email,password);
+});
+
+reg.addEventListener("click", function(event){
+  let email = document.getElementById("emailText").value;
+  let password = document.getElementById("passwordText").value;
+  regUser(email,password);
+});
